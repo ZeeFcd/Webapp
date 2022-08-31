b95cd1f0a82400d54b04009ca0b772809f19c5a0
ZeeFcd <ZeeFcd@protonmail.ch>
2022-09-01 00:48:50 +0200

    getting familiar with admin user

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Ticket
+from tinymce.widgets import TinyMCE
+from django.db import models
 
 # Register your models here.
+class TicketAdmin(admin.ModelAdmin):
+    fieldsets = [
+         ("Title/date", {'fields': ["ticket_title", "ticket_date"]}),
+         ("Content", {"fields": ["ticket_content"]})
+    ]
+
+    formfield_overrides = {
+         models.TextField: {'widget': TinyMCE()},
+    }
+
+
+admin.site.register(Ticket, TicketAdmin)
--- main/models.py
+++ main/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Ticket(models.Model):
     ticket_title = models.CharField(max_length=100)
     ticket_content = models.TextField()
-    ticket_date = models.DateTimeField('date published')
+    ticket_date = models.DateTimeField('date published', default=datetime.now)
 
     def __str__(self):
         return self.ticket_title
