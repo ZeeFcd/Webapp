7d4910c88e4fddf6ddafb2f5137cc198cf1c1045
ZeeFcd <ZeeFcd@protonmail.ch>
2022-08-31 18:32:06 +0200

    created a ticket model for testing out migration

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Ticket(models.Model):
+    ticket_title = models.CharField(max_length=100)
+    ticket_content = models.TextField()
+    ticket_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.ticket_title
